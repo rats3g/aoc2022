a5726be2d3dec7f6607de67ed6c468bf01181fe5
Robert Taylor <rats3g@gmail.com>
2022-12-01 09:13:15 -0500

    Solve day 1 part 2

--- 2022/1/Day1_2022.cs
+++ 2022/1/Day1_2022.cs
@@ -28,6 +28,27 @@ internal class Day1_2022 : ISolution
 
 public async Task<string> SolvePartTwo(string inputFile)
     {
-        return "unsolved";
+        var lines = File.ReadLinesAsync(inputFile);
+
+        var elves = new List<int>();
+        var current = 0;
+
+
+        await foreach (var line in lines)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                elves.Add(current);
+                current = 0;
+            }
+            else
+            {
+                current += int.Parse(line);
+            }
+        }
+
+        elves.Add(current);
+
+        return elves.OrderByDescending(x => x).Take(3).Sum(x => x).ToString();
     }
 }
