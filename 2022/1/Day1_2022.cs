5e2379faeeff9d064c795e6b9d9bc72d1c2db5a4
Robert Taylor <rats3g@gmail.com>
2022-12-01 09:09:36 -0500

    Solve day 1 part 1

--- 2022/1/Day1_2022.cs
+++ 2022/1/Day1_2022.cs
@@ -4,7 +4,26 @@ internal class Day1_2022 : ISolution
 {
     public async Task<string> SolvePartOne(string inputFile)
     {
-        return "unsolved";
+        var lines = File.ReadLinesAsync(inputFile);
+
+        var max = 0;
+        var current = 0;
+        
+
+        await foreach (var line in lines)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                max = Math.Max(max, current);
+                current = 0;
+            }
+            else
+            {
+                current += int.Parse(line);
+            }
+        }
+
+        return Math.Max(max, current).ToString();
     }
 
 public async Task<string> SolvePartTwo(string inputFile)
