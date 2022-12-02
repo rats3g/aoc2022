cfd887d194c6f316b3b81f2c083e8525e3da6c2f
Robert Taylor <rats3g@gmail.com>
2022-12-02 10:21:14 -0500

    Solve day 2

--- 2022/2/Day2_2022.cs
+++ 2022/2/Day2_2022.cs
@@ -4,11 +4,53 @@ internal class Day2_2022 : ISolution
 {
     public async Task<string> SolvePartOne(string inputFile)
     {
-        return "unsolved";
+        var lookup = new Dictionary<(string, string), int>
+        {
+            { ("A", "X"), 4 }, // Rock-Rock tie
+            { ("B", "X"), 1 }, // Paper-Rock loss
+            { ("C", "X"), 7 }, // Scissors-Rock win
+            { ("A", "Y"), 8 }, // Rock-Paper win
+            { ("B", "Y"), 5 }, // Paper-Paper tie
+            { ("C", "Y"), 2 }, // Scissors-Paper loss
+            { ("A", "Z"), 3 }, // Rock-Scissors loss
+            { ("B", "Z"), 9 }, // Paper-Scissors win
+            { ("C", "Z"), 6 }, // Scissors-Scissors tie
+        };
+
+        return File.ReadLines(inputFile).Aggregate(0, (currentScore, line) =>
+        {
+            if (line.Split() is [var opponent, var player])
+            {
+                return currentScore + lookup[(opponent, player)];
+            }
+
+            return currentScore;
+        }).ToString();
     }
 
 public async Task<string> SolvePartTwo(string inputFile)
     {
-        return "unsolved";
+        var lookup = new Dictionary<(string, string), int>
+        {
+            { ("A", "X"), 3 }, // Rock-Scissors loss
+            { ("B", "X"), 1 }, // Paper-Rock lose
+            { ("C", "X"), 2 }, // Scissors-Paper lose
+            { ("A", "Y"), 4 }, // Rock-Rock tie
+            { ("B", "Y"), 5 }, // Paper-Paper tie
+            { ("C", "Y"), 6 }, // Scissors-Scissors tie
+            { ("A", "Z"), 8 }, // Rock-Paper win
+            { ("B", "Z"), 9 }, // Paper-Scissors win
+            { ("C", "Z"), 7 }, // Scissors-Rock win
+        };
+
+        return File.ReadLines(inputFile).Aggregate(0, (currentScore, line) =>
+        {
+            if (line.Split() is [var opponent, var player])
+            {
+                return currentScore + lookup[(opponent, player)];
+            }
+
+            return currentScore;
+        }).ToString();
     }
 }
