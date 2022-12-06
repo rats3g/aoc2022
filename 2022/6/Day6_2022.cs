3f132a2fcac75723ba97af7a755c7cec29636af1
Robert Taylor <rats3g@gmail.com>
2022-12-06 09:26:11 -0500

    Solve day 6

--- 2022/6/Day6_2022.cs
+++ 2022/6/Day6_2022.cs
@@ -4,11 +4,63 @@ internal class Day6_2022 : ISolution
 {
     public async Task<string> SolvePartOne(string inputFile)
     {
-        return "unsolved";
+        var input = await File.ReadAllTextAsync(inputFile);
+
+        var map = new Dictionary<char, int>();
+
+        var leftIndex = 0;
+        var rightIndex = 0;
+        while (rightIndex < input.Length) 
+        {
+            var rightChar = input[rightIndex];
+            map.TryGetValue(rightChar, out var rightCount);
+            map[rightChar] = rightCount + 1;
+
+            while (map[rightChar] > 1)
+            {
+                map[input[leftIndex]]--;
+                leftIndex++;
+            }
+
+            if (rightIndex - leftIndex + 1 >= 4)
+            {
+                return (rightIndex + 1).ToString();
+            }
+
+            rightIndex++;
+        }
+
+        return "failure";
     }
 
     public async Task<string> SolvePartTwo(string inputFile)
     {
-        return "unsolved";
+        var input = await File.ReadAllTextAsync(inputFile);
+
+        var map = new Dictionary<char, int>();
+
+        var leftIndex = 0;
+        var rightIndex = 0;
+        while (rightIndex < input.Length)
+        {
+            var rightChar = input[rightIndex];
+            map.TryGetValue(rightChar, out var rightCount);
+            map[rightChar] = rightCount + 1;
+
+            while (map[rightChar] > 1)
+            {
+                map[input[leftIndex]]--;
+                leftIndex++;
+            }
+
+            if (rightIndex - leftIndex + 1 >= 14)
+            {
+                return (rightIndex + 1).ToString();
+            }
+
+            rightIndex++;
+        }
+
+        return "failure";
     }
 }
