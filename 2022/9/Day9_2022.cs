9270addbf3dddcb601353db1465fd43ca97f768a
Robert Taylor <rats3g@gmail.com>
2022-12-09 13:34:55 -0500

    Generate files for day 9

---
+++ 2022/9/Day9_2022.cs
@@ -0,0 +1,14 @@
+namespace AdventOfCode;
+
+internal class Day9_2022 : ISolution
+{
+    public async Task<string> SolvePartOne(string inputFile)
+    {
+        return "unsolved";
+    }
+
+    public async Task<string> SolvePartTwo(string inputFile)
+    {
+        return "unsolved";
+    }
+}
---
+++ 2022/9/Day9_2022Should.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace AdventOfCode;
+
+[Trait("Year", "2022")]
+[Trait("Day", "9")]
+public class Day9_2022Should
+{
+    [Fact]            
+    public void HaveATest()
+    {
+        
+    }
+}
