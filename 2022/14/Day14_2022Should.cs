039c712c1e446b99ebac6c0f772304d19480c5ff
Robert Taylor <rats3g@gmail.com>
2022-12-14 10:34:24 -0500

    Generate files for day 14

---
+++ 2022/14/Day14_2022Should.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace AdventOfCode;
+
+[Trait("Year", "2022")]
+[Trait("Day", "14")]
+public class Day14_2022Should
+{
+    [Fact]            
+    public void HaveATest()
+    {
+        
+    }
+}
