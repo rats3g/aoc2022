3f22b228ff426c756ea904e7ba5454b75ad0e139
rats3g <rats3g@gmail.com>
2022-12-15 10:25:24 -0500

    Generate files for day 15

---
+++ 2022/15/Day15_2022Should.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace AdventOfCode;
+
+[Trait("Year", "2022")]
+[Trait("Day", "15")]
+public class Day15_2022Should
+{
+    [Fact]            
+    public void HaveATest()
+    {
+        
+    }
+}
